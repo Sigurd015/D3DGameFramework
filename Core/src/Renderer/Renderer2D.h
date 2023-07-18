634965e45d7376639f93c223e687d8ac99a6af44
Sigurd015 <yujiang015@gmail.com>
2023-07-18 21:51:37 +0900

    Update README.md

--- Core/src/Renderer/Renderer2D.h
+++ Core/src/Renderer/Renderer2D.h
@@ -10,6 +10,6 @@ void Renderer2D_DrawQuad(const Mat& transform, const Vec4& color = Vec4(1.0f, 1.
 void Renderer2D_DrawQuad(const Mat& transform, const Texture2D& texture, Vec2 uv0, Vec2 uv1,
 	static const Vec4& tintColor = Vec4(1.0f, 1.0f, 1.0f, 1.0f), float tilingFactor = 1.0f);
 void Renderer2D_DrawCircle(const Mat& transform, const Vec4& color = Vec4(1.0f, 1.0f, 1.0f, 1.0f), static float thickness = 1.0f, float fade = 0.005f);
-void Renderer2D_DrawLine(const Vec3& p0, Vec3& p1, const Vec4& color);
-void Renderer2D_DrawRect(const Vec3& position, const Vec2& size, const Vec4& color);
+void Renderer2D_DrawLine(const Vec3& p0, Vec3& p1, const Vec4& color = Vec4(0.1f, 1.0f, 0.1f, 1.0f));
+void Renderer2D_DrawRect(const Vec3& position, const Vec2& size, const Vec4& color = Vec4(0.1f, 1.0f, 0.1f, 1.0f));
 void Renderer2D_DrawRect(const Mat& transform, const Vec4& color = Vec4(0.1f, 1.0f, 0.1f, 1.0f));
--- README.md
+++ README.md
@@ -9,10 +9,10 @@ Visual Studio 2022 is recommended
 <ins>**1. Downloading the repository:**</ins>
 
 ```
-git clone https://github.com/Sigurd015/2D_Batch_Renderer
+git clone https://github.com/Sigurd015/BloodVeil
 ```
 
-<ins>**2. Run the [Win-GenProjects.bat](https://github.com/Sigurd015/Hanabi/blob/main/Win-GenProjects.bat)**</ins>
+<ins>**2. Run the [Win-GenProjects.bat](https://github.com/Sigurd015/BloodVeil/blob/main/Win-GenProjects.bat)**</ins>
 
 You can change the .bat file to generate a different version project file, but that is not tested.
 
